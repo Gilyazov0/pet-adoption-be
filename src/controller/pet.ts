912df239168305df01082e42d40abaff3654c588
Sergey Gilyazov <s.gilyazov@gmail.com>
2023-01-04 14:32:53 +0200

    turned pet  controllers into class

--- src/controller/pet.ts
+++ src/controller/pet.ts
@@ -2,30 +2,32 @@ import { RequestHandler } from "express";
 import SearchParams from "../Types/searchParams";
 import { getPetByIdModel, searchModel, addPetModel } from "../model/pet";
 
-export const getPetById: RequestHandler = async (req, res) => {
-  const id = Number(req.query.id);
+export default class PetController {
+  public static getPetById: RequestHandler = async (req, res) => {
+    const id = Number(req.query.id);
 
-  const pet = await getPetByIdModel(id);
-  res.send(pet);
-};
-
-export const addPet: RequestHandler = async (req, res) => {
-  req.body.picture = req.file ? req.file.path : "";
-  req.body.height = Number(req.body.height);
-  req.body.weight = Number(req.body.weight);
-  req.body.hypoallergenic = Boolean(req.body.hypoallergenic);
-  req.body.adoptionStatus = "Available";
-
-  const pet = await addPetModel(req.body);
-  res.send(pet);
-};
-
-export const search: RequestHandler = async (req, res) => {
-  const params: SearchParams = {
-    ...req.query,
-    weight: Number(req.query.weight),
-    height: Number(req.query.height),
+    const pet = await getPetByIdModel(id);
+    res.send(pet);
   };
-  const pets = await searchModel(params);
-  res.send(pets);
-};
+
+  public static addPet: RequestHandler = async (req, res) => {
+    req.body.picture = req.file ? req.file.path : "";
+    req.body.height = Number(req.body.height);
+    req.body.weight = Number(req.body.weight);
+    req.body.hypoallergenic = Boolean(req.body.hypoallergenic);
+    req.body.adoptionStatus = "Available";
+
+    const pet = await addPetModel(req.body);
+    res.send(pet);
+  };
+
+  public static search: RequestHandler = async (req, res) => {
+    const params: SearchParams = {
+      ...req.query,
+      weight: Number(req.query.weight),
+      height: Number(req.query.height),
+    };
+    const pets = await searchModel(params);
+    res.send(pets);
+  };
+}
--- src/router/pet.ts
+++ src/router/pet.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { search, addPet, getPetById } from "../controller/pet";
+import PetController from "../controller/pet";
 import { isAdmin } from "../middleware/userMiddleware";
 import upload from "../middleware/imagesMiddleware";
 
 const router = Router();
 
-router.get("/id", getPetById);
-router.get("/search", search);
-router.post("/addPet", isAdmin, upload.single("picture"), addPet);
+router.get("/id", PetController.getPetById);
+router.get("/search", PetController.search);
+router.post("/addPet", isAdmin, upload.single("picture"), PetController.addPet);
 
 export default router;
